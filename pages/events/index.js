8a10df44265871d3e1df5aa0bfe5d1e0538e70d8
shoma hirao <shoma.hirao.rw@ambl.co.jp>
2023-04-19 13:36:41 +0900

    ♻️ テストデータを読み込んでイベント一覧ページを表示するようにした

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,22 +1,29 @@
 import Link from "next/link";
+import Image from "next/image";
 
-const EventsPage = () => {
+const EventsPage = ({ data }) => {
   return (
     <div>
       <h1>Events us Page</h1>
       <div>
-        <Link href="">
-          <h2> Events in London</h2>
-        </Link>
-        <Link href="">
-          <h2> Events in San Francisco</h2>
-        </Link>
-        <Link href="">
-          <h2> Events in Barcelona</h2>
-        </Link>
+        {data.map((ev) => (
+          <Link key={ev.id} href={"/events/${ev.id}"}>
+            <Image width={300} height={300} alt={ev.title} src={ev.image} />
+            <h2>{ev.title}</h2>
+          </Link>
+        ))}
       </div>
     </div>
   );
 };
 
 export default EventsPage;
+
+export async function getStaticProps() {
+  const { events_categories } = await import("/data/data.json");
+  return {
+    props: {
+      data: events_categories,
+    },
+  };
+}
