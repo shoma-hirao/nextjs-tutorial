a194105875018c52ebe13c4409caf4b69879dd70
shoma hirao <shoma.hirao.rw@ambl.co.jp>
2023-04-20 17:28:32 +0900

    ✨ イベントページ追加

---
+++ pages/events/[category]/[id].js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const EventsDetailPage = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <Image src={data.image} width={1000} height={500} alt={data.title} />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
+};
+
+export default EventsDetailPage;
+
+export async function getStaticPaths() {
+  const { allEvents } = await import("/data/data.json");
+  const allPaths = allEvents.map((path) => {
+    return {
+      params: {
+        category: path.city,
+        id: path.id,
+      },
+    };
+  });
+  console.log(allPaths);
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  console.log(context);
+  const { allEvents } = await import("/data/data.json");
+  const id = context?.params.id;
+  const eventData = allEvents.find((ev) => ev.id === id);
+  return {
+    props: { data: eventData },
+  };
+}
