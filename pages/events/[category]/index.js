31b1505cd462b8156b48d70b100ea4d67289006e
shoma hirao <shoma.hirao.rw@ambl.co.jp>
2023-04-20 15:29:53 +0900

    ✨テストデータを使ってイベント一覧ページがレンダリングされるようにした

--- pages/events/[category]/index.js
+++ pages/events/[category]/index.js
@@ -1,18 +1,53 @@
 import Link from "next/link";
+import Image from "next/image";
 
-const EventsCategoryPage = () => {
+const EventsCategoryPage = ({ data }) => {
   return (
     <div>
       <h1>Events in London</h1>
-
-      <Link href="/event/event1"> Event 1 </Link>
-      <Link href="/event/event2"> Event 2 </Link>
-      <Link href="/event/event3"> Event 3 </Link>
-      <Link href="/event/event4"> Event 4 </Link>
-      <Link href="/event/event5"> Event 5 </Link>
-      <Link href="/event/event6"> Event 6 </Link>
+      <div>
+        {data.map((ev) => (
+          <Link key={ev.id} href={`/events/${ev.city}/${ev.id}`}>
+            <Image width={300} height={300} alt={ev.title} src={ev.image} />
+            <h2>{ev.title}</h2>
+            <p>{ev.description}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default EventsCategoryPage;
+
+export async function getStaticPaths() {
+  const { events_categories } = await import("/data/data.json");
+  const allPaths = events_categories.map((ev) => {
+    return {
+      params: {
+        category: ev.id.toString(),
+      },
+    };
+  });
+  // console.log(allPaths);
+
+  return {
+    paths: allPaths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  // console.log(context);
+  const id = context?.params.category;
+  const { allEvents } = await import("/data/data.json");
+  // console.log(`id:${id}`);
+  const data = allEvents.filter((ev) => ev.city === id);
+  // console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
