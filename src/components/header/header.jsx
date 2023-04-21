0aba20ccecad17b70e4233042da0969f5d21fb79
shoma hirao <shoma.hirao.rw@ambl.co.jp>
2023-04-21 15:32:12 +0900

    💄 フッターとヘッターをかっこいい見た目にした

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export const Header = () => (
   <header>
-    <nav>
-      <Link href="/"> Home</Link>
-      <Link href="/events"> Events</Link>
-      <Link href="/about-us"> About Us</Link>
-    </nav>
+    <div className="topNav">
+      <Image alt="logo" src={"/images/logo_black.png"} width={50} height={50} />
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+          <li>
+            <Link href="/about-us">About Us</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <h1>Sed ut perspiciatis unde omnis</h1>
   </header>
 );
