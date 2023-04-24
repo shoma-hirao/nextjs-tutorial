2a2e9215212400d0345eecf61ad8c3f569b1854a
shoma hirao <shoma.hirao.rw@ambl.co.jp>
2023-04-25 07:02:14 +0900

    💄 ヘッダーのレイアウト調整

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,22 +3,29 @@ import Image from "next/image";
 
 export const Header = () => (
   <header>
-    <div className="topNav">
-      <Image alt="logo" src={"/images/logo_black.png"} width={50} height={50} />
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/events">Events</Link>
-          </li>
-          <li>
-            <Link href="/about-us">About Us</Link>
-          </li>
-        </ul>
-      </nav>
+    <div>
+      <div className="topNav">
+        <Image
+          alt="logo"
+          src={"/images/logo_black.png"}
+          width={50}
+          height={50}
+        />
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/events">Events</Link>
+            </li>
+            <li>
+              <Link href="/about-us">About Us</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <h1>Sed ut perspiciatis unde omnis</h1>
     </div>
-    <h1>Sed ut perspiciatis unde omnis</h1>
   </header>
 );
