b4f88f282a351c86e8d41f43c07c972c18a295aa
shoma hirao <shoma.hirao.rw@ambl.co.jp>
2023-04-25 08:11:56 +0900

    ♻️ イベントページのコンポーネント化

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,20 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
+import AllEvents from "@/src/components/events/events-page";
 
 const EventsPage = ({ data }) => {
-  return (
-    <div>
-      <h1>Events us Page</h1>
-      <div>
-        {data.map((ev) => (
-          <Link key={ev.id} href={`/events/${ev.id}`}>
-            <Image width={300} height={300} alt={ev.title} src={ev.image} />
-            <h2>{ev.title}</h2>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+  return <AllEvents data={data} />;
 };
 
 export default EventsPage;
